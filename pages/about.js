206ef2fa7849e2ea7e9ffd7e87b2f2ad3d3387a3
shehanp12 <shehanp068@gmail.com>
2020-11-04 00:08:43 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = () => (
+     <div>
+         <h1>About BitzPrize</h1>
+         <p>Application to view Bitcoin prices</p>
+     </div>
+ );
+
+export default About;
